7562106678954ae1644251ddbb407e466ba004d8
Silver Savi <silver.savi@gmail.com>
2020-01-26 17:39:32 +0200

    added Vendor API backend

--- src/test/java/guru/springfamework/services/CustomerServiceImplIT.java
+++ src/test/java/guru/springfamework/services/CustomerServiceImplIT.java
@@ -6,6 +6,7 @@ import guru.springfamework.bootstrap.Bootstrap;
 import guru.springfamework.domain.Customer;
 import guru.springfamework.repositories.CategoryRepository;
 import guru.springfamework.repositories.CustomerRepository;
+import guru.springfamework.repositories.VendorRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -27,6 +28,9 @@ public class CustomerServiceImplIT {
     CustomerRepository customerRepository;
 
     @Autowired
+    VendorRepository vendorRepository;
+
+    @Autowired
     CategoryRepository categoryRepository;
 
     CustomerService customerService;
@@ -37,7 +41,7 @@ public class CustomerServiceImplIT {
         System.out.println(customerRepository.findAll().size());
 
         //setup data for testing
-        Bootstrap bootstrap = new Bootstrap(categoryRepository, customerRepository);
+        Bootstrap bootstrap = new Bootstrap(categoryRepository, customerRepository, vendorRepository);
         bootstrap.run(); //load data
 
         customerService = new CustomerServiceImpl(CustomerMapper.INSTANCE, customerRepository);
